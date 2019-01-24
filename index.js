6d112f9d77c5d641be2d199d53ce4f3be903ca94
Minaul <47003983+Minaul@users.noreply.github.com>
2019-01-24 18:48:58 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+
+var bot = new Discord.Client();
+
+bot.on("ready", function(){
+    bot.user.setGame("Gypno, g:help");
+    console.log("Le bot a bien été connecté");
+});
+
+bot.login("NTM4MDQxMjk1OTUyNjc0ODI2.DyuEDQ.BkTD6vrYsIfOsVkrFe2V4fzjoao");
